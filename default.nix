cbb32284f975aff50f4a284fce4f41b5b4e5135c
kef <_@_>
2022-03-30 17:47:04 +1000

    _

--- default.nix
+++ default.nix
@@ -7,13 +7,5 @@ let
 in
   buildGradle {
     envSpec = ./gradle-env.json;
-
     src = ./.;
-
-    gradleFlags = [ "installDist" ];
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r app/build/install/myproject $out
-    '';
   }
