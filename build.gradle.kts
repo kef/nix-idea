513a403d4dc39c1ed8fc56511996e36880971207
kef <_@_>
2022-05-02 11:36:08 +1000

    _

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.changelog.closure
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.grammarkit.tasks.GenerateLexer
 import org.jetbrains.grammarkit.tasks.GenerateParser
@@ -7,7 +6,7 @@ plugins {
     // Java support
     id("java")
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "0.7.3"
+    id("org.jetbrains.intellij") version "1.5.3"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "0.6.2"
     // grammarkit - read more: https://github.com/JetBrains/gradle-grammar-kit-plugin
@@ -16,7 +15,6 @@ plugins {
 
 // Import variables from gradle.properties file
 val pluginGroup: String by project
-val pluginName: String by project
 val pluginVersion: String by project
 val pluginSinceBuild: String by project
 val pluginUntilBuild: String by project
@@ -47,10 +45,10 @@ dependencies {
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName = pluginName
-    version = platformVersion
-    type = platformType
-    updateSinceUntilBuild = true
+    pluginName.set(property("pluginName") as String)
+    version.set(platformVersion)
+    type.set(platformType)
+    updateSinceUntilBuild.set(true)
 }
 
 changelog {
@@ -75,7 +73,6 @@ sourceSets {
 }
 
 tasks {
-
     task("metadata") {
         outputs.upToDateWhen { false }
         doLast {
@@ -112,41 +109,34 @@ tasks {
     }
 
     patchPluginXml {
-        version(pluginVersion)
-        sinceBuild(pluginSinceBuild)
-        untilBuild(pluginUntilBuild)
+        version.set(pluginVersion)
+        sinceBuild.set(pluginSinceBuild)
+        untilBuild.set(pluginUntilBuild)
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription(
-                closure {
-                    File("./README.md").readText().lines().run {
-                        val start = "<!-- Plugin description -->"
-                        val end = "<!-- Plugin description end -->"
+        pluginDescription.set(provider {
+            File("./README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
 
-                        if (!containsAll(listOf(start, end))) {
-                            throw GradleException("Plugin description section not found in README.md file:\n$start ... $end")
-                        }
-                        subList(indexOf(start) + 1, indexOf(end))
-                    }.joinToString("\n").run { markdownToHTML(this) }
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md file:\n$start ... $end")
                 }
-        )
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").run { markdownToHTML(this) }
+        })
 
         // Get the latest available change notes from the changelog file
-        changeNotes(
-                closure {
-                    changelog.getLatest().toHTML()
-                }
-        )
+        changeNotes.set(provider { changelog.getLatest().toHTML() })
     }
 
     runPluginVerifier {
-        ideVersions(pluginVerifierIdeVersions)
-        failureLevel(org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.ALL)
+        ideVersions.set(pluginVerifierIdeVersions.split(","))
+        failureLevel.set(org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel.ALL)
     }
 
     publishPlugin {
-        token(System.getenv("JETBRAINS_TOKEN"))
-        channels(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
+        token.set(System.getenv("JETBRAINS_TOKEN"))
+        channels.set(listOf(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first()))
     }
-
 }
